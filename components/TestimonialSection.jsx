2c8c3e7fefead4d273c9ae1a141feec89bd232ac
Tushar Gaur <gaur9139@gmail.com>
2025-02-21 12:08:55 +0530

    pulled latest code'

--- components/TestimonialSection.jsx
+++ components/TestimonialSection.jsx
@@ -1,6 +1,5 @@
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import OwlCarousel from "react-owl-carousel";
 
 export default function TestimonialSection() {
   return (
