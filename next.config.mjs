f4dab3426841b145deefee95a2c893a8407bd3ac
Developer's Community - NMP <developercommunity@nationalmarketingprojects.com>
2025-03-03 15:25:17 +0530

    merging

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // ✅ Enables the App Router
+    },
+  };
+  
+  export default  nextConfig;
