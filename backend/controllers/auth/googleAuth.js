6f84f9b81a771df6d70e604679c8e3a8535c5a20
Developer's Community - NMP <developercommunity@nationalmarketingprojects.com>
2025-03-06 15:01:18 +0530

    update

--- backend/controllers/auth/googleAuth.js
+++ backend/controllers/auth/googleAuth.js
@@ -10,13 +10,14 @@ const googleCallback = passport.authenticate("google", {
 // Handle Google Auth Success & Store User in Database
 const handleGoogleCallback = async (req, res) => {
   if (!req.user) {
+    console.log("12345")
     return res.redirect(`${BASE_URL}/login`,); // Redirect on failure
   }
 
   try {
     // Extract user data from Google profile
     const { id, displayName, emails, photos } = req.user;
-
+    console.log("jdfjsnjn")
     // Check if user already exists in database
     let user = await User.findOne({ googleId: id });
 
