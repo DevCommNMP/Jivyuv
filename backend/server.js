66d9637553811a7ee90055ccd41814ed41cd12d2
Developer's Community - NMP <developercommunity@nationalmarketingprojects.com>
2025-03-06 11:33:17 +0530

    updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = 5000;
 const routes = require("./routes/allRoutes/allRoutes");
 const reviewRoutes = require("./routes/reviewRoutes/reviewRoutes");
 // Allowed Origins
-const allowedOrigins = ["http://localhost:3000","http://localhost:5173","http://localhost:5174", "https://accounts.google.com"];
+const allowedOrigins = ["https://jivyuv-tan.vercel.app","http://localhost:3000","http://localhost:5173","http://localhost:5174", "https://accounts.google.com"];
 
 const corsOptions = {
   origin: function (origin, callback) {
