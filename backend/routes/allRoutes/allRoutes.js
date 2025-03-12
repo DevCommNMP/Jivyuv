8bb395c6621f489198f98be304a809a82f13d9da
Aditya Singh <singhaditya2763@gmail.com>
2025-03-12 15:35:48 +0530

    updated navbar

--- backend/routes/allRoutes/allRoutes.js
+++ backend/routes/allRoutes/allRoutes.js
@@ -7,6 +7,8 @@ const reviewRoutes = require("../reviewRoutes/reviewRoutes");
 const categoryRoutes = require("../categoryRoutes/categoryRoutes");
 const subCategory = require("../subCategoryRoutes/subCategoryRoutes");
 const companyProfileRoutes = require("../companyProfileRoutes/companyProfileRoutes");
+const bannerRoutes = require("../bannerRoutes/bannerRoutes");
+const queryRoutes=require("../queryRoutes/queryRoutes");
 // Use Routes
 router.use("/api/auth", googleAuthRoutes); // All Google Auth routes under "/api/auth"
 router.use("/api", emailAuthRoutes); // All Google Auth routes under "/api/auth"
@@ -14,4 +16,6 @@ router.use("/api", reviewRoutes);
 router.use("/api",categoryRoutes );
 router.use("/api",subCategory)
 router.use("/api",companyProfileRoutes)
+router.use("/api",bannerRoutes)
+router.use("/api",queryRoutes)
 module.exports = router;
