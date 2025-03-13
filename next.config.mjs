4a4805448dd755b733e4c77d3587b4170b4be480
kundansca <anandg@maviator.com>
2025-03-13 12:35:09 +0530

    nothing change-kudan

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true, // ✅ Enables the App Router
-    },
-  };
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
   
   export default  nextConfig;
