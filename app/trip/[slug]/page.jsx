f9de455b87c4abf8b30c62dc647648445581a603
kundansca <anandg@maviator.com>
2025-03-19 18:11:42 +0530

    popular tour Section completed

--- app/trip/[slug]/page.jsx
+++ app/trip/[slug]/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,74 +12,28 @@ export default function TourDetails({params}) {
   const [isLoading,setIsLoading]=useState(false);
  
  
-  const [packageData, setPackageData] = useState({
-    _id: "67d91e71afd46c2b7fdb53f1",
-    title: "Kashmir Spring",
-    titleSlug: "kashmir-spring",
-    pickupLocation: "Srinagar",
-    dropLocation: "Srinagar",
-    numberOfDays: "10",
-    numberOfNights: "9",
-    overview: "Kashmir is & will always be the heaven on earth & for all the right reasons because, Travelling across this place is what dreams our made off! Srinagar the capital city of Kashmir is also known as the 'mini Switzerland of India' and that is because of the blooming vibrant plantation found here, especially the Tulips. The best part? You will be able to witness these plantations in person in this Kashmir tour.\n\nThis is a trip that is the perfect balance between leisure & adventure. From enjoying shikara rides and houseboat accommodation on Nigeen lake in Srinagar to exploring off beat places.\n\nOn this journey, we also set out to traverse across South Kashmir. The list definitely includes Saffron fields of Pampore along with meadows of Aru & Betab Valley in Pahalgam and something no backpacker can forget to explore- Gulmarg. All year round, Gulmarg is in its full glory. A Gandola ride & visit to Affarwat Peak is a must!\n\nJoin us on the adventure to backpack in this heaven on earth to enjoy the warm hospitality of people and the astonishing gems of nature.",
-    packagePrice: "24999",
-    packagePromotional: "29999",
-    isVisaFree: true,
-    country: "india",
-    categoryId: "67d91c18afd46c2b7fdb5368",
-    subCategoryId: "67d91c67afd46c2b7fdb5378",
-    tripTagName: "popular",
-    startingDate: "Sat Mar 29 2025 16:47:00 GMT+0530 (India Standard Time)",
-    isPickupAndDropAvailable: true,
-    packageImage: "uploads/packageImages/1742282353402/packageImage/1742282353403-packageImage_resized.jpeg",
-    packageSubImages: [],
-    activityData: [
-        {
-            activityDay: "01",
-            activityTitle: "Srinagar Arrival | Local Sightseeing of Srinagar",
-            activityDescription: [
-                "Arrive at the Srinagar airport after a scenic flight over the snow-capped mountains.",
-                "From Srinagar airport you will have to reach your houseboat and check in.",
-                "Have some rest and have your lunch."
-            ],
-            _id: "67d91e71afd46c2b7fdb53f2"
-        },
-        {
-            activityDay: "2",
-            activityTitle: "Srinagar To Gulmarg. Enjoy Asia’s Highest Gandola Ride",
-            activityDescription: [
-                "Wake up in the morning, have your breakfast and get ready for the day.",
-                "We will then take you to Gulmarg - that appears straight out of painting."
-            ],
-            _id: "67d91e71afd46c2b7fdb53f3"
-        },
-        {
-            activityDay: "3",
-            activityTitle: "Transfer to Pahalgam I Overnight Stay in Pahalgam",
-            activityDescription: [
-                "After breakfast in the morning, get ready to begin your day.",
-                "Board your vehicle as we will take you to Pahalgam which will be a 4-hour scenic drive via Pampore and Avantipura"
-            ],
-            _id: "67d91e71afd46c2b7fdb53f4"
-        }
-    ],
-    createdAt: "2025-03-18T07:19:13.490Z",
-    __v: 0
-}
+  const [packageData, setPackageData] = useState(
+   
 );
+let x={
+  trending:[],
+  popular:[],
+  
+}
 
  async function fetchPackageData() {
     setIsLoading(true);
     try {
       let response =await  axios.get(`${SERVER_URL}/api/trip-packages/slug/${slug}`);
-      console.log("response data");
+      
       console.log(response.data);
       setPackageData(response.data);
     } catch (error) {
       console.log(error);
       Swal.fire({
         icon:"error",
-        text:"Something went wrong"
-      });
+        text:error.response.data.message || "Something went wrong"
+      })
     }finally{
       setIsLoading(false);
     }
@@ -94,7 +49,7 @@ export default function TourDetails({params}) {
       {/* Page Title */}
       <section
         className="page-title style-three"
-        style={{ backgroundImage: `url(${SERVER_URL}/${packageData.packageImage})` }}
+        style={{ backgroundImage: `url(${SERVER_URL}/${packageData?.packageImage})` }}
       >
         <div className="auto-container">
           <div className="inner-box">
@@ -105,7 +60,7 @@ export default function TourDetails({params}) {
             </div>
             <h2>Moscow Red City Land</h2>
             <h3>
-              {packageData.packagePrice}<span> / Per person</span>
+              {packageData?.packagePrice}<span> / Per person</span>
             </h3>
           </div>
         </div>
@@ -130,15 +85,15 @@ export default function TourDetails({params}) {
                     <ul className="info-list clearfix">
                       <li>
                         <span style={{fontWeight:"bolder"}}>Pickup & Drop</span><br/>
-                        <i className="far fa-clock">{packageData.isPickupAndDropAvailable==true?<span style={{fontWeight:"bold"}}>{packageData.pickupLocation}-{packageData.dropLocation}</span>:"Any Time"}</i>
+                        <i className="far fa-clock">{packageData?.isPickupAndDropAvailable==true?<span style={{fontWeight:"bold"}}>{packageData?.pickupLocation}-{packageData?.dropLocation}</span>:"Any Time"}</i>
                       </li>
                       <li>
                       <span style={{fontWeight:"bolder"}}>Duration</span><br/>
-                        <i className="far fa-user"><span style={{fontWeight:"bold"}}>{packageData.numberOfNights}N - {packageData.numberOfDays}D</span></i>
+                        <i className="far fa-user"><span style={{fontWeight:"bold"}}>{packageData?.numberOfNights}N - {packageData?.numberOfDays}D</span></i>
                       </li>
                       <li>
                       <span style={{fontWeight:"bolder"}}>Country</span><br/>
-                        <i className="far fa-map"><span style={{fontWeight:"bold"}}>{packageData.country}</span></i>
+                        <i className="far fa-map"><span style={{fontWeight:"bold"}}>{packageData?.country}</span></i>
                       </li>
                     </ul>
                   </div>
@@ -147,25 +102,25 @@ export default function TourDetails({params}) {
                 {/* Tour Plan */}
                 <div className="tour-plan">
                   <div className="text">
-                    <h2>{packageData.title}</h2>
+                    <h2>{packageData?.title}</h2>
                     <p style={{textAlign:"justify"}}>
-                     {packageData.overview}
+                     {packageData?.overview}
                     </p>
                   </div>
                   
-                 {packageData.activityData.map((activity)=>{
+                 {packageData?.activityData?.map((activity)=>{
 
                     return <div className="content-box">
                     <div className="single-box">
-                      <span>{activity.activityDay}</span>
+                      <span>{activity?.activityDay}</span>
                       {/* <h4>8:00 am to 10:00 am</h4> */}
-                      <h3>{activity.activityTitle}</h3>
+                      <h3>{activity?.activityTitle}</h3>
                       {/* <p>
                         Aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                         fugiat nulla pariatur.
                       </p> */}
                       <ul className="list clearfix">
-                        {activity.activityDescription.map((list)=>{
+                        {activity?.activityDescription?.map((list)=>{
                           return  <li style={{textAlign:"justify"}}>{list}</li>
 
                         })
@@ -192,21 +147,20 @@ export default function TourDetails({params}) {
                    
                   </div>
                   <div className="image-box clearfix">
-                    {packageData.packageSubImages.map((img)=>{
-                      return <figure className="image">
-                      
-                      
-                      <Image
+                 
+                    {packageData?.packageSubImages?.map((img)=>{
+                      return  <figure className="image"> <Image
                         src={`${SERVER_URL}/${img}`}
                         alt="Gallery Image"
                         width={300}
                         height={200}
                       />
                    
-                    
-                    </figure>
+                   </figure>
+                   
 
                     })
+                  
                     // <figure className="image">
                       
                       
@@ -283,5 +237,7 @@ export default function TourDetails({params}) {
         </div>
       </section>
     </div>
+    
+    
   );
 }
--- components/TourSection.jsx
+++ components/TourSection.jsx
@@ -1,8 +1,57 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 
 import { Clock, Map, Star } from "lucide-react";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import axios from 'axios';
+import { useRouter } from "next/navigation";
+
+
 export default function TourSection() {
+  const [packageData,setPackageData]=useState({popular:[],trending:[],offer:[]});
+  const [loading,setIsLoading]=useState();
+  const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+      const router = useRouter();
+  
+  async function fetchPackageData(){
+    let tagName={popular:[],trending:[],offer:[]}
+    try{
+   let response=await axios.get(`${SERVER_URL}/api/trip-packages`);
+     let  data=response.data.reverse();
+     data.forEach((item)=>{
+    
+      if(item.tripTagName==="popular"){
+
+       item.tripTagName.length!==3 && tagName.popular.push(item);
+        
+      }else if(item.tripTagName==="trending"){
+        item.tripTagName.length!==3 && tagName.trending.push(item);
+      }else if(item.tripTagName==="offer"){
+        item.tripTagName.length!==3 && tagName.offer.push(item);
+      }
+
+     })
+   
+      setPackageData(tagName);
+
+    }catch(error){
+      console.log(error);
+      Swal.fire({icon:"error", title:error?.response?.message || "We’re facing some issues fetching the data.Please try again."});
+
+    }finally{
+
+    }
+  }
+  useEffect(()=>{
+    fetchPackageData();
+
+  },[])
+  const handleNavigation = (slug) => {
+    router.push(`/trip/${slug}`);
+  };
+
   return (
     <>
       <section className="tour-section bg-color-1 sec-pad">
@@ -16,57 +65,77 @@ export default function TourSection() {
             <h2>Our Most Popular Adventures</h2>
           </div>
           <div className="row clearfix">
-            <div className="col-lg-4 col-md-6 col-sm-12 tour-block">
-              <div
-                className="tour-block-one wow fadeInUp animated animated"
-                data-wow-delay="00ms"
-                data-wow-duration="1500ms"
-              >
-                <div className="inner-box">
-                  <figure className="image-box">
-                    <Image
-                      src="/assets/img/home/tour-1.jpg"
-                      alt=""
-                      width={370}
-                      height={270}
-                    />
-                    <Link href="/trip/[slug]" as="/trip/moscow-red-city-land">
-                      <i className="fas fa-link"></i>
-                    </Link>
-                  </figure>
-                  <div className="lower-content">
-                    <div className="rating">
-                      <span>
-                        <Star size={24} color={"gold"} /> 8.0 Superb
-                      </span>
-                    </div>
-                    <h3>
-                      <Link href="/trip/[slug]" as="/trip/moscow-red-city-land">
-                        Moscow Red City Land
-                      </Link>
-                    </h3>
-                    <h4>
-                      $170.00<span> / Per person</span>
-                    </h4>
-                    <ul className="info clearfix">
-                      <li>
-                        <Clock size={24} /> 5 Days
-                      </li>
-                      <li>
-                        <Map size={24} /> G87P, Birmingham
-                      </li>
-                    </ul>
-                    <p>Lorem ipsum dolor amet consectetur adipiscing sed.</p>
-                    <div className="btn-box">
-                      <Link href="/trip/[slug]" as="/trip/moscow-red-city-land">
-                        See Details
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 col-sm-12 tour-block">
+          
+              {packageData?.popular?.map((trip)=>{
+return    <div className="col-lg-4 col-md-6 col-sm-12 tour-block">
+<div
+ className="tour-block-one wow fadeInUp animated animated"
+ data-wow-delay="00ms"
+ data-wow-duration="1500ms"
+>
+ <div className="inner-box">
+   <figure className="image-box">
+     <Image
+       src={`${SERVER_URL}/${trip?.packageImage}`}
+       alt=""
+       width={370}
+       height={270}
+     />
+     <Link href={`/api/trip-packages/slug/${trip.titleSlug}`} as={`/api/trip-packages/slug/${trip.titleSlug}`} onClick={(event)=>{
+        event.preventDefault();
+        handleNavigation(trip.titleSlug);
+
+       }}>
+       <i className="fas fa-link"></i>
+     </Link>
+   </figure>
+   <div className="lower-content">
+     <div className="rating">
+       <span>
+         <Star size={24} color={"gold"} /> 8.0 Superb
+       </span>
+     </div>
+     <div style={{height:"130px",}}>
+     <h3 style={{fontSize:"16px",wordSpacing:'1px',fontWeight:"600",letterSpacing:"1px"}}>
+     <Link href={`/api/trip-packages/slug/${trip.titleSlug}`} as={`/api/trip-packages/slug/${trip.titleSlug}`} onClick={(event)=>{
+        event.preventDefault();
+        handleNavigation(trip.titleSlug);
+
+       }}>
+         {trip.title}
+       </Link>
+     </h3>
+     </div>
+     <h4 style={{marginBottom:"4px",marginTop:"15px"}}>
+     ₹ {trip.packagePrice}<span> / Per person</span>
+     </h4>
+     <ul className="info clearfix">
+       <li>
+         <Clock size={24} /> <span>{trip.numberOfNights}N - {trip.numberOfDays}D</span>
+       </li>
+       <li>
+         <Map size={24} /> <span style={{textTransform:"uppercase"}}>{trip.country}</span>
+       </li>
+     </ul>
+   
+     <div className="btn-box">
+       <Link href={`/api/trip-packages/slug/${trip.titleSlug}`} as={`/api/trip-packages/slug/${trip.titleSlug}`} onClick={(event)=>{
+        event.preventDefault();
+        handleNavigation(trip.titleSlug);
+
+       }}>
+         See Details
+       </Link>
+     </div>
+   </div>
+ </div>
+</div>
+</div>
+              })
+             
+             }
+           
+            {/* <div className="col-lg-4 col-md-6 col-sm-12 tour-block">
               <div
                 className="tour-block-one wow fadeInUp animated animated"
                 data-wow-delay="300ms"
@@ -165,7 +234,7 @@ export default function TourSection() {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
