87cf1fcecd5777ab477197c70ea5fe1c44afef6c
kundansca <anandg@maviator.com>
2025-05-02 16:02:04 +0530

    reduce network request

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@
 
 import Footer from "../components/FooterSection";
 import Header from "../components/HeaderSection";
+import React from "react";
 import "./custom.css";
 import "./globals.css";
 import * as Lucide from 'lucide-react';
@@ -13,40 +14,48 @@ export const metadata = {
   title: "Tour & Travel",
   description: "Tour & Travel",
 };
-
+export let seoContextObj=React.createContext();
 
 
 
 export default function RootLayout({ children }) {
   const [categories, setCategories] = useState([]);
   const [companyData, setCompanyData] = useState([]);
+  const [packageData,setPackageData]=useState();
   const [isLoading, setIsLoading] = useState(false);
 
+
   useEffect(() => {
-    fetchCategoriesAndCompanyProfile(setIsLoading, setCategories, setCompanyData);
+    fetchCategoriesAndCompanyProfile(setIsLoading, setCategories, setCompanyData,setPackageData);
   }, []);
 
   // Ensure Preloader does not replace the entire DOM structure
   if (isLoading) {
     return (
+      <seoContextObj.Provider value={{categories:categories, companyData:companyData, packageData:packageData}}>
+
+
       <html lang="en" suppressHydrationWarning>
         <body>
           <Preloader />
         </body>
       </html>
+    </seoContextObj.Provider>
     );
   }
   return (
+    <seoContextObj.Provider value={{categories:categories, companyData:companyData,packageData:packageData}}>
     <html lang="en" suppressHydrationWarning>
          
 
       <body>
 
-        <Header categories={categories} companyData={companyData} />
+        <Header  />
         {children}
         <Footer companyData={companyData} categories={categories} />
         {/* <script src="//code.tidio.co/jle9bquxji2qq9f15iuurgscznud28e9.js" async></script> */}
       </body>
     </html>
+    </seoContextObj.Provider>
   );
 }
